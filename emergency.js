151460669f5c090172bb18cb9034fb9333a4f4df
Jayantosocial <jayanto.inovate@gmail.com>
2025-09-08 01:57:56 +0600

    call button alert done

--- emergency.js
+++ emergency.js
@@ -1,14 +1,25 @@
-// Get all heart icons
 const hearts = document.getElementsByClassName('heart');
-// Get the element that shows the heart count
 const heartCount = document.getElementById('heartcount');
 let count = 0;
 
-// Loop through each heart and attach click event
 for (const heart of hearts) {
     heart.addEventListener('click', function() {
-        count++; // increment counter
+        count++;
         heartCount.innerText = count; // update the counter in the navbar
         console.log('Heart clicked! Total:', count);
+    });
+}
+
+
+const callButtons = document.getElementsByClassName('button2');
+const serviceNames = document.getElementsByClassName('service-name');
+const phoneNumbers = document.getElementsByClassName('phone-number');
+
+for (let i = 0; i < callButtons.length; i++) {
+    callButtons[i].addEventListener('click', function () {
+        const serviceName = serviceNames[i].innerText;
+        const serviceNumber = phoneNumbers[i].innerText;
+
+        alert(`Calling ${serviceName} at ${serviceNumber}`);
     });
 }
